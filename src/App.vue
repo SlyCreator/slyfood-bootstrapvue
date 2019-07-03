a6d68285c96f22443301f35805c528389e4b5616
slycreator <chinonso1amaechi@gmial.com>
2019-07-03 22:48:01 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <Cards />
+  </div>
+</template>
+<script>
+import Navbar from "./components/Navbar.vue";
+import Cards from "./components/Cards";
+export default {
+  name: "navbar",
+  components: {
+    Navbar,
+    Cards
+  }
+};
+</script>
